d680e6f6f8520aa78e794e7bc451b65538712c6d
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-06 13:16:46 -0400

    added routes for html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const tableData = require('./data/tableData');
+const waitListData = require('./data/waitingListData');
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,10 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile('index')) 
+//html routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/home.html'))) 
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'public/reserve.html'))) 
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'public/tables.html'))) 
 
-app.get('/add', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/add.html'))
-})
+//api routes
+app.get('/api/tables', (req, res) => res.json(tableData));
+app.get('/api/waitlist', (req, res) => res.json(waitListData))
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
