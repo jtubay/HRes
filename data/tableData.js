7f2f9296c5431f48128bdbc5c9bb019a6eb25798
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-06 11:27:39 -0400

    adding html

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+const tableArray = [
+    {
+        customerName: "Ahmed",
+        customerEmail: "ahmed@mail.com",
+        customerID: "afhaque89",
+        phoneNumber: "00-000-000"
+    }
+];
+
+module.exports = tableArray;
---
+++ data/waitingListData.js
@@ -0,0 +1,9 @@
+const waitingArray = [
+    {
+        customerName: "Saima",
+        customerEmail: "saima@example.com",
+        phoneNumber: "000-000-0000",
+        customerID: "saimaCool"
+    }
+]
+module.exports = waitingArray;
